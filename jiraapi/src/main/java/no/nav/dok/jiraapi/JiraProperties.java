a0d68782db36e7e8696be7dc19dcfba9ea802613
Tsigab <tsigab.angosom.gebremedhin@nav.no>
2024-10-07 09:39:21 +0200

    MMA-7580 fjernet proxy

--- jiraapi/src/main/java/no/nav/dok/jiraapi/JiraProperties.java
+++ jiraapi/src/main/java/no/nav/dok/jiraapi/JiraProperties.java
@@ -4,19 +4,18 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 /**
- *
  * @param jiraServieUser jira brukernavn og passord
- * @param url  jira url
+ * @param url            jira url
  */
 
 @Builder
-public record JiraProperties(JiraServieUser jiraServieUser, @NotEmpty String url, Proxy proxy) {
+public record JiraProperties(JiraServieUser jiraServieUser, @NotEmpty String url) {
 
 	/**
 	 * @param username applikasjonceller bruker jira brukernavn
 	 * @param password applikasjonen eller brukeren jira passord
 	 */
-	public record JiraServieUser(@NotEmpty String username, @NotEmpty String password) { }
+	public record JiraServieUser(@NotEmpty String username, @NotEmpty String password) {
+	}
 
-	public record Proxy(String host, int port) { }
 }
