ef183738e015d23d0ad05b976c5a2f4bee90a52f
Tsigab <tsigab.angosom.gebremedhin@nav.no>
2024-10-17 10:19:24 +0200

    MMA-7580 endret inputstream til byte array

--- jiraapi/src/main/java/no/nav/dok/jiraapi/client/JiraClient.java
+++ jiraapi/src/main/java/no/nav/dok/jiraapi/client/JiraClient.java
@@ -89,7 +89,6 @@ public class JiraClient {
 		try {
 			HttpRequest httpRequest = httpRequestBuilder()
 					.uri(URI.create(jiraProperties.url() + ISSUE_PATH + "/" + key + ATTACHMENT))
-					.header(CONTENT_TYPE, APPLICATION_OCTET_STREAM_VALUE)
 					.POST(HttpRequest.BodyPublishers.ofByteArray(convertFileToByteArray(request.file())))
 					.build();
 			HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
